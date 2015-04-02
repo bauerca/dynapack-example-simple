f3798b360b26090460d181390d66083c1a13eeed
Carl Bauer <carl.a.bauer@gmail.com>
2015-04-01 20:52:36 -0700

    Break off layout from server.js for clarity

---
+++ layout.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+var scripts = fs.readFileSync('bundles/app.html', {encoding: 'utf8'});
+var words = 'hey do you want to know a secret';
+
+function layout(content) {
+  return (
+    '<html>' +
+    '<head>' +
+      '<style>li {cursor: pointer;} li:hover {color: #3366bb;}</style>' +
+    '</head>' +
+    '<body>' +
+      '<h1>Find the secret</h1>' +
+      '<ul>' +
+      words.split(' ').map(function(word) {
+        return '<li onclick="go(\'/' + word + '\')">secret</li>';
+      }).join('') +
+      '</ul>' +
+      '<div id="content">' + content + '</h1>' +
+      scripts +
+    '</body>' +
+    '</html>'
+  );
+}
+
+module.exports = layout;
--- server.js
+++ server.js
@@ -1,32 +1,8 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-var route = require('./route');
 var parseurl = require('parseurl');
-var fs = require('fs');
-
-var scripts = fs.readFileSync('bundles/app.html', {encoding: 'utf8'});
-var words = 'hey do you want to know a secret';
-
-// Page layout.
-function renderString(content) {
-  return (
-    '<html>' +
-    '<head>' +
-      '<style>li {cursor: pointer;} li:hover {color: #3366bb;}</style>' +
-    '</head>' +
-    '<body>' +
-      '<h1>Find the secret</h1>' +
-      '<ul>' +
-      words.split(' ').map(function(word) {
-        return '<li onclick="go(\'/' + word + '\')">secret</li>';
-      }).join('') +
-      '</ul>' +
-      '<div id="content">' + content + '</h1>' +
-      scripts +
-    '</body>' +
-    '</html>'
-  );
-}
+var route = require('./route');
+var layout = require('./layout');
 
 var app = connect();
 
@@ -36,7 +12,7 @@ app.use('/images', serveStatic(__dirname + '/images'));
 
 app.use(function(req, res) {
   route(parseurl(req).path, function(err, content) {
-    var html = renderString(err ? err.message : content);
+    var html = layout(err ? err.message : content);
     res.end(html);
   });
 });
